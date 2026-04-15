b38bf56f04e4b6a71266f3299007de52e18b7a52
Eduardo R <dudub9go@hotmail.com>
2026-04-15 17:58:49 -0300

    initial commit

---
+++ src/main/java/com/watch_inventory/watch_inventory/WatchInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.watch_inventory.watch_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchInventoryApplication.class, args);
+	}
+
+}
